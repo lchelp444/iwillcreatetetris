971ffc31bea2f1fbc9e47f845c9aa2c3839c3128
Ivan Biba <protivanime@gmail.com>
2017-05-12 21:24:31 +0300

    cool bugs

--- src_sample/ConsolEngine/TestApp.cpp
+++ src_sample/ConsolEngine/TestApp.cpp
@@ -49,9 +49,11 @@ void TestApp::KeyPressed(int btnCode)
 //		mObj1Y++;
       break;
    case 75:
+	   mMoveLeft=1;
 //		mObj1X--;
       break;
    case 77:
+	   mMoveRight=1;
 //		mObj1X++;
       break;
    case 32:
@@ -93,7 +95,8 @@ void TestApp::UpdateF(float deltaTime)
 
    int newX = mOldX + mMoveRight - mMoveLeft;
    int newY = mOldY + 1;
-
+	mMoveRight=0;
+		mMoveLeft=0;
    for (int i=0; i<4; i++)
       for (int j=0; j<2; j++)
          SetChar(mOldX + i, mOldY + j, L'.');
